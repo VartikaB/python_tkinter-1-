b7e594a47e0a732c7331df4e0d6a09e5415f7115
Vartika <bhardwajvartika9450@gmail.com>
2022-01-01 18:50:57 +0530

    commit1

---
+++ dice.py
@@ -0,0 +1,13 @@
+import random
+from tkinter import *
+root=Tk()
+root.geometry("700x450")
+l1=Label(root,text='',font=("times",200))
+def roll():
+    number=['\u2680','\u2861','\u2682','\u2684','\u2685']
+    l1.config(text=f'{random.choice(number)}')
+    l1.pack()
+
+b1=Button(root,text="roll the dice....",command=roll)
+b1.place(x=330,y=0)
+root.mainloop()
